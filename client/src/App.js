e289700ef7af5a33a921aa3ee6841c2c6bb0d113
raytellier <rayzesports@gmail.com>
2020-08-26 02:20:57 -0400

    :sparkles: Added Mapbox w Markers & Popups

---
+++ client/src/App.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+
+import { listLogEntries } from "./API";
+
+const App = () => {
+  const [logEntries, setLogEntries] = useState([]);
+  const [showPopup, setShowPopup] = useState({});
+  const [viewport, setViewport] = useState({
+    width: "100vw",
+    height: "100vh",
+    latitude: 37.6,
+    longitude: -95.665,
+    zoom: 3,
+  });
+
+  useEffect(() => {
+    (async () => {
+      const logEntries = await listLogEntries();
+      setLogEntries(logEntries);
+    })();
+  }, []);
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      mapStyle="mapbox://styles/raytellier/cke388cx904w419o5i2pxtdc7"
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      onViewportChange={setViewport}
+    >
+      {logEntries.map((entry) => (
+        <>
+          <Marker
+            key={entry._id}
+            latitude={entry.latitude}
+            longitude={entry.longitude}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <div
+              onClick={() =>
+                setShowPopup({
+                  [entry._id]: true,
+                })
+              }
+            >
+              <svg
+                className="marker"
+                style={{
+                  width: `${6 * viewport.zoom}px`,
+                  height: `${6 * viewport.zoom}px`,
+                }}
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                <circle cx="12" cy="10" r="3"></circle>
+              </svg>
+            </div>
+          </Marker>
+          {showPopup[entry._id] ? (
+            <Popup
+              latitude={entry.latitude}
+              longitude={entry.longitude}
+              closeButton={true}
+              closeOnClick={false}
+              dynamicPosition={true}
+              onClose={() => setShowPopup({})}
+              anchor="top"
+            >
+              <div className="popup">
+                <h3>{entry.title}</h3>
+                <p>{entry.comments}</p>
+              </div>
+            </Popup>
+          ) : null}
+        </>
+      ))}
+    </ReactMapGL>
+  );
+};
+
+export default App;
